6481e5e86419a208d65fc56c8f954187ed948137
Luca Rager <lucarager@gmail.com>
2025-10-26 11:42:25 -0700

    Fix some bugs

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -36,25 +36,59 @@ import { ModRegistrar } from "cs2/modding";
  */
 
 const register: ModRegistrar = (moduleRegistry) => {
+    const debugUIClasses = moduleRegistry.registry.get(
+        "game-ui/debug/components/debug-ui.module.scss",
+    )?.classes;
+    const scrollableClasses = moduleRegistry.registry.get(
+        "game-ui/common/scrolling/scrollable.module.scss",
+    )?.classes;
+    const panelBackdropClasses = moduleRegistry.registry.get(
+        "game-ui/common/panel/panel-backdrop.module.scss",
+    )?.classes;
 
-    const debugUIClasses = moduleRegistry.registry.get("game-ui/debug/components/debug-ui.module.scss")?.classes;
-    const scrollableClasses = moduleRegistry.registry.get("game-ui/common/scrolling/scrollable.module.scss")?.classes;
-
-    var style = document.createElement('style');
-    style.type = 'text/css';
+    const style = document.createElement("style");
+    style.type = "text/css";
     style.innerHTML = `
         .${debugUIClasses["debug-ui"]} {
             top: 0;
             left: 0;
             bottom: 0;
-            right: 0;
+            right: 5vw;
+            display: flex;
+            flex-direction: row;
+        }
+
+        body.${panelBackdropClasses["backdrop-visible"]} .${debugUIClasses["debugging"]} {
+            opacity: 0.5;
+            pointer-events: none !important;
+        }
+
+        body.${panelBackdropClasses["backdrop-visible"]} .${debugUIClasses["debugging"]} * {
+            pointer-events: none !important;
         }
 
         .${debugUIClasses["inspector"]} {
-            width: 1200rem;
-            max-width: 80vw;
             flex-direction: row;
+            width: 66%;
         }
+
+        .${debugUIClasses["output-column"]} {
+            width: 33%;
+        }
+
+        .${debugUIClasses["output-column"]} .${debugUIClasses["scrollable"]} {
+            height: 100%;
+        }
+
+        .${debugUIClasses["output-column"]} .${scrollableClasses["content"]} {
+            height: 100%;
+        }
+
+        .${debugUIClasses["output-column"]} .${debugUIClasses["output"]} {
+            min-height: 100%;
+            background: rgba(14, 18, 36, 0.82);
+        }
+
 
         .${debugUIClasses["tab-bar"]} {
             flex-direction: column;
@@ -62,16 +96,28 @@ const register: ModRegistrar = (moduleRegistry) => {
             padding-left: 0;
             padding-right: 0;
             width: 200rem;
+            overflow-x: auto;
+            overflow-y: scroll;
+            flex-wrap: nowrap;
+        }
+
+        .${debugUIClasses["tab-bar"]}: {
+            flex-direction: column;
+            background-color: rgba(0, 0, 0, 0.25);
+            padding-left: 0;
+            padding-right: 0;
+            width: 200rem;
+            flex-wrap: nowrap;
         }
 
         .${debugUIClasses["tab-bar"]} > button {
             border: none;
             text-align: left;
-            font-size: 1.1em;
+            font-size: 1em;
             padding-left: 16rem;
             padding-right: 16rem;
-            padding-top: 6rem;
-            padding-bottom: 6rem;
+            padding-top: 4rem;
+            padding-bottom: 4rem;
             font-weight: 600;
         }
 
@@ -90,7 +136,7 @@ const register: ModRegistrar = (moduleRegistry) => {
         }
     `;
 
-    document.getElementsByTagName('head')[0].appendChild(style);
-}
+    document.getElementsByTagName("head")[0].appendChild(style);
+};
 
 export default register;
